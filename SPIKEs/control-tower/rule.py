67cb4a9c646347aa187d04ec63a5c2fdabbb6a05
Jason Smigovsky <jsmigovsky@fearless.tech>
2022-01-12 15:06:40 -0500

    IA-2158: refine script to make evaluation

--- SPIKEs/control-tower/rule.py
+++ SPIKEs/control-tower/rule.py
@@ -1,75 +1,116 @@
 import boto3
 import json
-def get_client(service):
-  return boto3.client(service)
 
 def check_defined(reference, reference_name):
   if not reference:
     raise Exception('Error: ', reference_name, 'is not defined')
   return reference
 
-def is_oversized_changed_notification(message_type):
-    check_defined(message_type, 'messageType')
-    return message_type == 'OversizedConfigurationItemChangeNotification'
+def check_security_group_rules(groupId):
+  e = boto3.resource('ec2')
+  sg = e.SecurityGroup(groupId)
 
-def get_configuration_item(invokingEvent):
-  check_defined(invokingEvent, 'invokingEvent')
-  if is_oversized_changed_notification(invokingEvent['messageType']):
-    configurationItemSummary = check_defined(invokingEvent['configurationItemSummary'], 'configurationItemSummary')
-    return get_configuration(configurationItemSummary['resourceType'], configurationItemSummary['resourceId'], configurationItemSummary['configurationItemCaptureTime'])
-  return check_defined(invokingEvent['configurationItem'], 'configurationItem')
+  is_compliant = True
 
-def is_applicable(configurationItem, event):
-  try:
-    check_defined(configurationItem, 'configurationItem')
-    check_defined(event, 'event')
-  except:
-    return True
-  status = configurationItem['configurationItemStatus']
-  eventLeftScope = event['eventLeftScope']
-  if status == 'ResourceDeleted':
-    print("Resource Deleted, setting Compliance Status to NOT_APPLICABLE.")
-  return (status == 'OK' or status == 'ResourceDiscovered') and not eventLeftScope
+  # check for non-compliacne ports, etc...
+  for rule in sg.ip_permissions:
+    # restrict: SSH:22
+    # restrict: RDP:3389
+    if rule['FromPort'] == rule['ToPort'] and rule['FromPort'] in [22, 3389]:
+      is_compliant = False
+      for range in rule['IpRanges']:
+        print(f'NonCompliantRule - FromPort: {rule["FromPort"]}, ToPort: {rule["ToPort"]}, Rnage: {range["CidrIp"]}')
+      for range in rule['Ipv6Ranges']:
+        print(f'NonCompliantRule - FromPort: {rule["FromPort"]}, ToPort: {rule["ToPort"]}, Rnage: {range["CidrIpv6"]}')
 
-def evaluate_change_notification_compliance(configuration_item, rule_parameters):
-  check_defined(configuration_item, 'configuration_item')
-  check_defined(configuration_item['configuration'], 'configuration_item[\'configuration\']')
-  if rule_parameters:
-    check_defined(rule_parameters, 'rule_parameters')
-  if (configuration_item['resourceType'] != 'AWS::EC2::Instance'):
-    return 'NOT_APPLICABLE'
-  elif rule_parameters.get('desiredInstanceType'):
-    if (configuration_item['configuration']['instanceType'] in rule_parameters['desiredInstanceType']):
-      return 'COMPLIANT'
-  return 'NON_COMPLIANT'
+    # anywhere?
+    # only: http:80
+    # only: https:443
+    for range in rule['IpRanges']:
+      if range['CidrIp'] in ['0.0.0.0/0']:
+        if rule['FromPort'] not in [80, 443] or rule['ToPort'] not in [80, 443]:
+          is_compliant = False
+          print(f'NonCompliantRule - FromPort: {rule["FromPort"]}, ToPort: {rule["ToPort"]}, Rnage: {range["CidrIp"]}')
+    for range in rule['Ipv6Ranges']:
+      if range['CidrIpv6'] in ['::/0']:
+        if rule['FromPort'] not in [80, 443] or rule['ToPort'] not in [80, 443]:
+          is_compliant = False
+          print(f'NonCompliantRule - FromPort: {rule["FromPort"]}, ToPort: {rule["ToPort"]}, Rnage: {range["CidrIpv6"]}')
+  # if no traps catch the rule then we can simply return as compliant
+  if is_compliant:
+    return 'COMPLIANT'
+  else:
+    return 'NON_COMPLIANT'
 
 def lambda_handler(event, context):
-  root = check_defined(event, 'invokingEvent');
-  ie    = json.loads(root['invokingEvent'])
-  item  = ie['configurationItem']
-  mtype = ie['messageType']
-  print(f'AccountId:    {item["awsAccountId"]}')
-  print(f'ResourceId:   {item["resourceId"]}')
-  print(f'ResourceName: {item["resourceName"]}')
-  print(f'ResourceType: {item["resourceType"]}')
-  print(f'ARN:          {item["ARN"]}')
-  print(f'Region:       {item["awsRegion"]}')
-  print(f'AZ:           {item["availabilityZone"]}')
-  print(f'MessageType:  {mtype}')
 
-  #TODO: confirm is security group resource
-  #TODO: evaluate inbound rule for ssh:22 and rdp:3389 as they are not compliant
-  #TODO: handle response
-  response = AWS_CONFIG_CLIENT.put_evaluations(
+  # print out the entire event for troubleshooting purposes
+  print('Printing raw JSON event for troubleshooting purposes')
+  print(json.dumps(event, indent=2))
+
+  # check to see if the invokingEvent element is present or throw an exception
+  print('Checking for invokingEvent element')
+  check_defined(event, 'invokingEvent');
+
+  # parse the invokingEvent field as JSON data
+  print('Parsing invokingEvent JSON data')
+  invokingEvent = json.loads(event['invokingEvent'])
+
+  # check to see if the invokingElement contains a configurationItem element or throw an exception
+  print('Checking for configurationItem element')
+  check_defined(invokingEvent, 'configurationItem')
+
+  # ez access to configurationItem
+  print('Getting handle to configurationItem element')
+  item = invokingEvent["configurationItem"]
+
+  # ez access to configurationItem properties
+  az = item["availabilityZone"]
+  arn = item["ARN"]
+  region = item["awsRegion"]
+  status = item["configurationItemStatus"]
+  accountId = item["awsAccountId"]
+  resourceId = item["resourceId"]
+  messageType = invokingEvent['messageType']
+  captureTime = item["configurationItemCaptureTime"]
+  resourceName = item["resourceName"]
+  resourceType = item["resourceType"]
+
+  # display resource information on the resource that triggered the event
+  print(f'AZ:           {az}')
+  print(f'ARN:          {arn}')
+  print(f'AccountId:    {accountId}')
+  print(f'ResourceId:   {resourceId}')
+  print(f'ResourceName: {resourceName}')
+  print(f'ResourceType: {resourceType}')
+  print(f'Region:       {region}')
+  print(f'Status:       {status}')
+  print(f'MessageType:  {messageType}')
+
+  # set a default compliance type for the response
+  compliance_type = 'NOT_APPLICABLE'
+
+  # check if the resource is being deleted
+  if status != "ResourceDeleted":
+    # based on the resource type take an appropriate action
+    if resourceType == "AWS::EC2::SecurityGroup":
+      compliance_type = check_security_group_rules(resourceId)
+    else:
+      raise Exception('Error: ', resourceType, 'is not a supported resource type, please modify your code')
+
+  print(compliance_type)
+
+  config = boto3.client('config')
+  response = change.put_evaluations(
     Evaluations=[
       {
-        'ComplianceResourceType': invoking_event['configurationItem']['resourceType'],
-        'ComplianceResourceId': invoking_event['configurationItem']['resourceId'],
-        'ComplianceType': compliance_value,
-        'OrderingTimestamp': invoking_event['configurationItem']['configurationItemCaptureTime']
+        'ComplianceResourceType': resourceType,
+        'ComplianceResourceId': resourceId,
+        'ComplianceType': compliance_type,
+        'OrderingTimestamp': captureTime
       },
     ],
-     ResultToken=event['resultToken']
+    ResultToken=event['resultToken']
   )
 
 
