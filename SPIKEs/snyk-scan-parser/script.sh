673cc32d5c71cdbdd4a8c5221c90b1b219c1915b
Jason Smigovsky <jsmigovsky@fearless.tech>
2022-06-14 15:26:58 -0400

    IA-2500: working on example to search for and if necessary create and issue in JIRA

---
+++ SPIKEs/snyk-scan-parser/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -D- \
+-X GET \
+-H "Content-Type: application/json" \
+-u "john.dough@aol.com:AAAAAAAAAAAAAAAAAAAAAAAA" \
+https://us-sba.atlassian.net/rest/agile/1.0/board/22/backlog
+
